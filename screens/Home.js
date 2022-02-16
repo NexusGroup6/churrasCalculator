9d2aa3bc9899382009dd279c353761bd39ffabfe
nexus <nexus6group@gmail.com>
2022-02-16 15:50:41 -0300

    Button

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,24 @@
 import * as React from "react";
-import { View, Text, ImageBackground, StyleSheet, Alert } from "react-native";
-import { Button } from "react-native";
+import { View, Text, ImageBackground, StyleSheet} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import Calculadora from './calculadora';
 
-export default function Home() {
+const Stack = createStackNavigator();
+
+function MyStack(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Calculadora" component={Calculadora} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -14,11 +30,13 @@ export default function Home() {
         >
           <Text style={styles.title}>Bem Vindo!</Text>
           {/* <Button title='Calculadora' color={'red'}/> */}
-          <Button
-            title="Calculadora"
-            color='red'
-            onPress={() => Alert.alert("Indo à calculadora...")}
-          />
+
+          <TouchableOpacity style={styles.botao}
+          onPress={() => navigation.navigate("Calculadora")}
+          >
+            <Text style={styles.botao}>CALCULADORA</Text>
+          </TouchableOpacity>
+
         </View>
       </ImageBackground>
     </View>
@@ -30,6 +48,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
+  botao:{
+    fontWeight:'bold',
+    backgroundColor:'red',
+    fontSize:26,
+    textAlign:'center',
+    paddingTop:13,
+    paddingBottom:13,
+    paddingLeft:10,
+    paddingRight:10,
+    color:'white',
+    borderRadius:100
+  },
   img: {
     flex: 1,
     justifyContent: "center",
@@ -39,5 +69,6 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "700",
     color: "#ffff",
+    paddingBottom:15
   },
 });
