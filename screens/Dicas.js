44bb35da18bf67566dd58a195cf9ba5e052a5ca7
Nexus <nexus6group@gmail.com>
2022-02-17 16:39:56 -0300

    Page Dicas modificações

--- screens/Dicas.js
+++ screens/Dicas.js
@@ -1,10 +1,86 @@
-import * as React from 'react';
-import { View, Text } from "react-native";
+import * as React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function Dicas() {
-   return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text style={{fontSize:16,fontWeight:'700'}}>Dicas</Text>
-</View>
-   );
- }
+  return (
+    <View>
+      <Text style={styles.title}>Dicas</Text>
+      {/* Dica 1 */}
+      <View>
+        <Text style={styles.subtitle}>
+          1. Planeje a quantidade de comida e bebida para seu churrasco
+        </Text>
+        <Text style={styles.content}>
+          Utilizando o Churras Calculator, é possível saber a quantidade exata
+          de carne e bebida a ser comprada, assim ajudando no não desperdício e
+          para que não se gaste mais ou menos do que o necessário. Deste modo,
+          seu churrasco será melhor planejado e mais eficiente.
+        </Text>
+      </View>
+
+      {/* Dica 2 */}
+      <View>
+        <Text style={styles.subtitle}>
+          2. Use mais do que o sal grosso como tempero
+        </Text>
+        <Text style={styles.content}>
+          Uma grande dica para tornar seu churrasco ainda melhor é utilizar
+          outros temperos nas carnes e não somente o sal grosso. Para carnes
+          bovinas, é interessante usar o Dry Black Rub, que é um sal com
+          diversas especiarias para dar um gosto diferente à carne. Já para
+          carne de porco e frango é bom usar o Dry Rub, que é um tempero de
+          especiarias com um gosto muito marcante.
+        </Text>
+        <Image
+          style={styles.img}
+          source={require("../imgs/dry_hub_black.jpg")}
+        />
+      </View>
+
+      {/* Dica 3 */}
+      <View>
+        <Text style={styles.subtitle}>
+          3. Prepare alguns molhos para as carnes
+        </Text>
+        <Text style={styles.content}>
+          Os molhos são muito importantes no churrasco, tanto para fazer na
+          preparação das carnes, quanto para fazer com um molho para comer junto
+          com a carne pronta. Como dica para molhos para fazer na preparação do
+          churrasco são o barbecue e as mostardas. O barbecue é muito bom para
+          se colocar nos frangos ou na costela antes de colocar na grelha. Já a
+          mostarda é ótima para colocar nos frangos ou carne de porco antes,
+          entretanto também fica muito boa para se comer junto com a linguiça. O
+          molho de alho é um molho para se comer junto com a carne feita,
+          podendo se colocar em qualquer tipo.
+        </Text>
+        <Image
+          style={styles.img}
+          source={require("../imgs/molho.jpg")}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: "brown",
+    fontSize: 32,
+    padding: 20,
+    textAlign: "right",
+  },
+  subtitle: {
+    color: "red",
+    textAlign: "left",
+    paddingLeft: 15,
+  },
+  content: {
+    color: "blue",
+    textAlign: "justify",
+  },
+  img: {
+    marginTop: 25,
+    alignSelf: "center",
+    width: "100%",
+  },
+});
--- screens/calculadora.js
+++ screens/calculadora.js
@@ -1,70 +1,11 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { TextInput, TouchableOpacity } from 'react-native-web';
+import { Text, View } from 'react-native';
+
 
 export default function Calculadora() {
-   const [adulto, setAdulto] = useState(0);
-   const [resultado, setResultado] = useState(0);
-
-   const calcAdultos = () => {
-      setResultado(parseFloat(adulto) * 0.500);
-   }
-
-   return (
-      <>
-        
-         <View style={styles.container}>
-         <Text style={{ fontSize: 16, fontWeight: '700' }}>Calculadora</Text>
-            <StatusBar style="auto" />
-            <TextInput
-               style={styles.input}
-               onChangeText={(teste) => { setAdulto(teste) }}
-               value={String(adulto)}
-               placeholder="Quantidade de adultos"
-               keyboardType="numeric"
-            />
-
-            <TouchableOpacity
-               style={styles.button}
-               onPress={() => calcAdultos()}
-            >
-               <Text>Press Here</Text>
-            </TouchableOpacity>
-            <TextInput
-               value={String(resultado)}
-               onChangeText={(teste) => { setResultado(teste) }}
-            />
-         </View>
-      </>
-   );
-}
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
-   button: {
-      alignItems: "center",
-      backgroundColor: "#DDDDDD",
-      padding: 10
-   },
-   input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-   },
-});
-
-/*export default function Calculadora() {
    return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 <Text style={{fontSize:16,fontWeight:'700'}}>Calculadora</Text>
 </View>
    );
- }*/
+ }
